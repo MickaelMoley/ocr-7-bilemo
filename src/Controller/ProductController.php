61f2a700d39dc078d41474062c89a99c52dd4607
Mickael Moley <mickael.moley@outlook.com>
2021-09-17 12:14:28 +0200

    Fix #5 We can fetch list of products
    - Prefix route for with "/api" for all controllers
    - Install jms/serializer
    - Make function that return listof products in ProductController
    - Enable DI for Controller (because we dont use AbstractController in ProductController)

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/products")
+ */
+class ProductController
+{
+
+    /**
+     * Permet de récupérer la liste de tous les produits
+     * @Route(name="api_product_collection_get", format="json", methods={"GET"})
+     * @return JsonResponse
+     */
+    public function collection(ProductRepository $productRepository, SerializerInterface $serializer) : JsonResponse
+    {
+
+
+        return new JsonResponse(
+            $serializer->serialize($productRepository->findAll(), 'json'),
+            Response::HTTP_OK,
+            [],
+            true
+        );  
+       
+    }
+
+   
+}
